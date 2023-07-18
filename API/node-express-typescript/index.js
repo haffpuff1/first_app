dad6feeb65e8d94998710df81bf73a3803cc570d
rahaf rammal <rammalrahaf@gmail.com>
2023-07-18 12:24:13 +0300

    Updated index.js and index.ts files

--- API/node-express-typescript/index.js
+++ API/node-express-typescript/index.js
@@ -1,16 +1,70 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const validation_1 = require("./validation/validation");
+const new_player_1 = require("./src/components/new_player");
+const team_1 = require("./src/components/team");
+const summary_1 = require("./src/components/summary");
+const random_1 = require("./src/components/random");
+const high_APT_1 = require("./src/components/high_APT");
+const avg_1 = require("./src/components/avg");
+const player_schema_1 = require("./player_schema");
+const player_schema_2 = require("./player_schema");
+const player_schema_3 = require("./player_schema");
+const sort_APT_1 = require("./src/components/sort_APT");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server is running');
-});
+app.use(express_1.default.json());
+app.post('/new_Player', (0, validation_1.validate)(player_schema_1.create_new_player_schema), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, new_player_1.create_new_player)(req.body);
+    res.statusCode = 201;
+    res.send('New player created successfully.');
+}));
+app.get('/new_team', (0, validation_1.validate)(player_schema_2.create_new_team_schema), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, team_1.create_new_team)(req.body);
+    res.statusCode = 201;
+    res.send('New team created successfully.');
+}));
+app.get('/random_team', (0, validation_1.validate)(player_schema_3.random_team_schema), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield (0, random_1.random_team)(req.body);
+    res.statusCode = 201;
+    res.json(result);
+}));
+app.get('/get_Players', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield (0, summary_1.getPlayers)();
+    res.statusCode = 201;
+    res.json(result);
+}));
+app.get('/sort_by_APT', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield (0, sort_APT_1.sort_by_APT)();
+    res.statusCode = 201;
+    res.json(result);
+}));
+app.get('/highest_APT', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield (0, high_APT_1.highest_APT)();
+    res.statusCode = 201;
+    res.send(result);
+}));
+app.get('/lowest_avg', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield (0, avg_1.lowest_avg)();
+    res.statusCode = 201;
+    res.json(result);
+}));
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+// npm run dev
--- API/node-express-typescript/index.ts
+++ API/node-express-typescript/index.ts
@@ -1,15 +1,68 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { validate } from "./validation/validation";
+import { create_new_player } from "./src/components/new_player";
+import { Player } from "./src/components/player";
+import { create_new_team } from "./src/components/team";
+import { getPlayers } from "./src/components/summary";
+import { random_team } from "./src/components/random";
+import { highest_APT } from "./src/components/high_APT";
+import {lowest_avg} from "./src/components/avg";
+import { create_new_player_schema } from "./player_schema";
+import {create_new_team_schema} from "./player_schema";
+import {random_team_schema} from "./player_schema";
+import { team_info } from "./src/components/team_info";
+import { nb_req } from "./src/components/nb_req";
+import { sort_by_APT } from "./src/components/sort_APT";
 
 dotenv.config();
-
-const app: Express = express();
+const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server is running');
+app.use(express.json());
+
+app.post('/new_Player', validate(create_new_player_schema), async (req: Request, res: Response) => {
+  await create_new_player(req.body as Player);
+  res.statusCode = 201;
+  res.send('New player created successfully.');
 });
 
+app.get('/new_team', validate(create_new_team_schema), async (req: Request, res: Response) => {
+  await create_new_team(req.body as team_info);
+  res.statusCode = 201;
+  res.send('New team created successfully.');
+});
+
+app.get('/random_team', validate(random_team_schema), async (req: Request, res: Response)=> {
+  const result = await random_team(req.body as nb_req);
+  res.statusCode = 201;
+  res.json(result);
+});
+
+app.get('/get_Players' ,async(req: Request, res: Response) => {
+  const result = await getPlayers();
+  res.statusCode = 201;
+  res.json(result);
+
+});
+app.get('/sort_by_APT',async(req: Request, res: Response) => {
+  const result = await sort_by_APT();
+  res.statusCode = 201;
+  res.json(result);
+});
+
+app.get('/highest_APT', async (req: Request, res: Response) => {
+const result = await highest_APT();
+res.statusCode = 201;
+res.send(result);
+});
+
+app.get('/lowest_avg', async(req: Request, res: Response) => {
+  const result = await lowest_avg();
+  res.statusCode = 201;
+  res.json(result);
+});
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+// npm run dev
